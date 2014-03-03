2787401da444d327552c687b7bd580fddef18309
Juan Carlos Romero <juancarlos.romero@gmail.com>
2014-03-03 09:15:16 +0100

    BoardConfig cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,18 +31,6 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 
-# Mass storage
-BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
-
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 576716800
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 14906032128
-BOARD_FLASH_BLOCK_SIZE := 16384
-
-# Kernel
 TARGET_PREBUILT_KERNEL := device/bq/edison2qc/prebuilt/kernel
 BOARD_KERNEL_BASE := 0x60400000
 BOARD_KERNEL_PAGESIZE := 16384
@@ -52,14 +40,15 @@ BOARD_CUSTOM_BOOTIMG_MK := device/bq/edison2qc/mkbootimg.mk
 # Recovery
 TARGET_RECOVERY_FSTAB := device/bq/edison2qc/recovery.fstab
 TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+DEVICE_RESOLUTION := 1280x800
 RECOVERY_SDCARD_ON_DATA := true
-TW_FLASH_FROM_STORAGE := true
+TW_NO_SCREEN_TIMEOUT := true
 TW_INTERNAL_STORAGE_PATH := "/data/media"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-DEVICE_RESOLUTION := 1280x800
-TW_MAX_BRIGHTNESS := 255
 TW_BRIGHTNESS_PATH := /sys/devices/platform/rk29_backlight/backlight/rk28_bl/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
