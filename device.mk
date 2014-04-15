5ce02b3db3ff59cca6006cc72f0a40529886432d
J. Carlos Romero <juancarlos.romero@gmail.com>
2014-04-16 00:01:54 +0200

    Enable encryption of data partition

--- device.mk
+++ device.mk
@@ -10,6 +10,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/init:root/init \
     $(LOCAL_PATH)/prebuilt/rk30xxnand_ko.ko.3.0.36+:root/rk30xxnand_ko.ko.3.0.36+ \
     $(LOCAL_PATH)/init.recovery.rk30board.rc:root/init.recovery.rk30board.rc
+    $(LOCAL_PATH)/fstab.rk30board:root/fstab.rk30board
 
 $(call inherit-product, build/target/product/full.mk)
 
